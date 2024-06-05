56d99321061601c85f6588c90529495bbdcf526b
TrungLe19 <leductrung19022001@gmail.com>
2024-06-05 11:08:34 +0700

    Buổi 4: struct, enum

---
+++ Bai22.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BaiTapC_
+{
+    internal class Bai22
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            NhapChuoi(out str);
+            Console.WriteLine("Do dai chuoi: {0}", demDoDaiChuoi(str));
+
+            Console.ReadLine();
+        }
+
+        // Hàm nhập chuỗi
+        static void NhapChuoi(out string chuoi)
+        {
+            Console.Write("Nhap chuoi: ");
+            chuoi = Console.ReadLine();
+        }
+
+        // Hàm đếm độ dài chuỗi
+        int demDoDaiChuoi(* str)
+        {
+            int dem = 0;
+            while (*str != '\0')
+            {
+                dem++;
+                str++;
+            }
+            return dem;
+        }
+
+    }
+}
